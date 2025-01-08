754fbccf933177e2ea1fe48f63f86b549df56d6a
chuka <g00414637@atu.ie>
2025-01-08 21:10:07 +0000

    created files for transaction microservcie

---
+++ src/main/java/ie/atu/transferapp/TransactionController.java
@@ -0,0 +1,4 @@
+package ie.atu.transferapp;
+
+public class TransactionController {
+}
---
+++ src/test/java/ie/atu/transferapp/TransferAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.transferapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
